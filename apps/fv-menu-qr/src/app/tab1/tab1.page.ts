1161bcca10338fd1732503fe471a99eeb688292b
David Tobing <saddamtbg@gmail.com>
2022-08-05 13:05:08 +0700

    create as simple as possible template for qr-menu (#2)

--- apps/fv-menu-qr/src/app/tab1/tab1.page.ts
+++ apps/fv-menu-qr/src/app/tab1/tab1.page.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'fv-playing-qr-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class Tab1Page {
+
+  fvListMenus = [
+    {
+      name: 'Nasi',
+      imageUrl: 1
+    },
+    {
+      name: 'Snack',
+      imageUrl: 2
+    },
+    {
+      name: 'Kopi',
+      imageUrl: 3
+    },
+    {
+      name: 'Teh',
+      imageUrl: 4
+    },
+    {
+      name: 'Jus',
+      imageUrl: 5
+    },
+    {
+      name: 'Lainnya',
+      imageUrl: 6
+    }
+  ];
+
   constructor() {}
 
-  unread() {
-  }
 }
