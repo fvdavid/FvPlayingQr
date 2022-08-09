a99c37a7609b50b3f24fdd9f49d1b607a3a34eef
David Tobing <saddamtbg@gmail.com>
2022-08-09 09:25:47 +0700

    migrating from Nxtend to Nxext (#3)

--- apps/fv-menu-qr/jest.config.ts
+++ apps/fv-menu-qr/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'fv-menu-qr',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
--- libs/material/jest.config.ts
+++ libs/material/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'portal',
+/* eslint-disable */
+export default {
+  displayName: 'material',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/portal',
+  coverageDirectory: '../../coverage/libs/material',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
